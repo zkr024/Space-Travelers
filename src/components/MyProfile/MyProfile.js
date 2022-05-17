251c1a746f7cfe99178839f45442be86cef1ebb5
Sirvelasque <velasquezwalter10@yahoo.es>
2022-05-17 14:46:43 -0600

    Display completed mission in My profile

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,5 +1,22 @@
-const MyProfile = () => (
-  <div>My Profile</div>
+import { useSelector } from 'react-redux';
+
+const item = (t) => (
+  <div key={t.name}>{t.name}</div>
 );
+
+const MyProfile = () => {
+  const data = useSelector((state) => state);
+  const missions = data.mission.filter((task) => task.reserved === true);
+  return (
+    <div className="Main_Container">
+      <div className="Container">
+        <h2>My Missions</h2>
+        <div className="items">
+          {missions.map((mission) => item(mission))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
