b1fd5500ca78160b0f1d2c60f65b3ced416ea3b6
zkr024 <zkr.bm.02403@gmail.com>
2022-05-17 19:55:35 -0500

    apply changes

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,33 +1,69 @@
 import { useSelector } from 'react-redux';
 
-import './myProfile.css';
+import { FaRocket } from 'react-icons/fa';
 
-const item = (value) => (
-  <div
-    key={value.name}
-    className="panels"
-  >
-    {value.name}
-  </div>
-);
+import ListRocket from './ListRocket';
+import ListMission from './ListMission';
+import './style/myProfile.css';
+
+const item = (value) => {
+  const info = (
+    <ListRocket
+      id={value.id}
+      name={value.name}
+      key={value.id}
+    />
+  );
+  return info;
+};
+
+const itemTwo = (value) => {
+  const info = (
+    <ListMission
+      id={value.id}
+      name={value.name}
+      key={value.id}
+    />
+  );
+  return info;
+};
 
 const MyProfile = () => {
   const data = useSelector((state) => state);
   const missions = data.mission.filter((task) => task.reserved === true);
   const rockets = data.space.filter((task) => task.reserved === true);
+
   return (
     <div className="Main_Container">
       <div className="Container">
         <h2>My Missions</h2>
-        <div className="items">
-          {missions.map((mission) => item(mission))}
-        </div>
+        { missions.length === 0
+          ? (
+            <h3 className="noSelected">
+              <FaRocket className="iconFa" />
+              No missions selected
+            </h3>
+          )
+          : (
+            <div className="items">
+              {missions.map((mission) => itemTwo(mission))}
+            </div>
+          )}
       </div>
       <div className="Container">
         <h2>My Rockets</h2>
-        <div className="items">
-          {rockets.map((rocket) => item(rocket))}
-        </div>
+        { rockets.length === 0
+          ? (
+            <h3 className="noSelected">
+              <FaRocket className="iconFa" />
+              No rockets reserved
+            </h3>
+          )
+          : (
+            <div className="items">
+              {rockets.map((rockets) => item(rockets))}
+            </div>
+          )}
       </div>
     </div>
   );
